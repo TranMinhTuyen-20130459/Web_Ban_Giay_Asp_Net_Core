2749d652a090ae31eeffb8b72598f745eced57cf
Tuyên Minh <tmt010101ccna@gmail.com>
2023-07-30 07:57:14 +0700

    tạo kết nối đến MySQL

---
+++ Entities/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_Ban_Giay_Asp_Net_Core.Entities
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<TestTable> TestTables { get; set; }
+
+
+    }
+}
---
+++ Entities/TestTable.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_Ban_Giay_Asp_Net_Core.Entities
+{
+    [Table("TestTables")]
+    public class TestTable
+    {
+        [Key]
+        public int id_test { get; set; }
+
+        public string name_test { get; set; }
+        
+    }
+}
