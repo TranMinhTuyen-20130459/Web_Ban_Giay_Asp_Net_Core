ec01a3699a367250b2fa929b48fad66491a35794
Tuyên Minh <tmt010101ccna@gmail.com>
2023-08-02 09:12:50 +0700

    tạo function để thêm dữ liệu vào table sizes, type_products

--- Data/Class/DataSize.cs
+++ Data/Class/DataSize.cs
@@ -1,6 +1,39 @@
-﻿namespace Web_Ban_Giay_Asp_Net_Core.Data.Class
+﻿using System.Diagnostics;
+using Web_Ban_Giay_Asp_Net_Core.Data.Interface;
+using Web_Ban_Giay_Asp_Net_Core.Entities;
+using Web_Ban_Giay_Asp_Net_Core.Entities.Config;
+
+namespace Web_Ban_Giay_Asp_Net_Core.Data.Class
 {
-    public class DataSize
+    public class DataSize : IAddData
     {
+        public string[] arr_size = { "36", "37", "38", "39", "40", "41", "42", "43", "44" };
+        public void AddDataToTable(MyDbContext dbContext)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            using (var transaction = dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach (var item in arr_size)
+                    {
+                        var size = new Size { name_size = item };
+                        dbContext.Add(size);
+                        dbContext.SaveChanges();
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine(e.Message);
+                }
+            }
+            watch.Stop();
+            TimeSpan time = watch.Elapsed;
+            Console.WriteLine("Excute time: " + time.TotalSeconds + " s");
+        }
     }
 }
