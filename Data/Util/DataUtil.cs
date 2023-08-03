f32d63079f36adb44c23b0c59430653e5c20ecfb
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-08-03 18:32:05 +0700

    tạo function de them data vao bang image_products

--- Data/Util/DataUtil.cs
+++ Data/Util/DataUtil.cs
@@ -1,6 +1,34 @@
-﻿namespace Web_Ban_Giay_Asp_Net_Core.Data.Util
+﻿using System.Collections;
+using Web_Ban_Giay_Asp_Net_Core.Entities;
+using Web_Ban_Giay_Asp_Net_Core.Entities.Config;
+
+namespace Web_Ban_Giay_Asp_Net_Core.Data.Util
 {
     public class DataUtil
     {
+        public static void PrintArrayList(ArrayList arrayList)
+        {
+            foreach (var item in arrayList)
+            {
+                Console.Write(item + ",");
+            }
+        }
+
+        public static ArrayList GetListIdProduct(MyDbContext dbContext)
+        {
+            var result = new ArrayList();
+            try
+            {
+                // Sử dụng LINQ để chỉ chọn thuộc tính id_product từ bảng Products.
+                List<long> list_id_product = dbContext.Set<Product>().Select(p => p.id_product).ToList();
+                result = new ArrayList(list_id_product);
+            }
+            catch (Exception ex)
+            {
+                result = new ArrayList();
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
     }
 }
