7201f1847bafbe117b94f94c32aec88ebc5b31ae
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-08-04 17:26:19 +0700

    tạo function để thêm data vào table orders

--- Data/Class/DataOrder.cs
+++ Data/Class/DataOrder.cs
@@ -1,6 +1,119 @@
-﻿namespace Web_Ban_Giay_Asp_Net_Core.Data.Class
+﻿using System.Diagnostics;
+using Web_Ban_Giay_Asp_Net_Core.Data.Interface;
+using Web_Ban_Giay_Asp_Net_Core.Entities;
+using Web_Ban_Giay_Asp_Net_Core.Entities.Config;
+
+namespace Web_Ban_Giay_Asp_Net_Core.Data.Class
 {
-    public class DataOrder
+    public class DataOrder : IAddData
     {
+        public string[] arr_name_senders = { "MESSI", "RONALDO", "NEYMAR", "MBAPPE", "BENZEMA" };
+
+        public string[] arr_name_receivers = { "TUYEN 1", "TUYEN 2", "TUYEN 3", "TUYEN 4", "TUYEN 5" };
+
+        public string[] arr_email_customers = {"trantuyen.dev.1@gmail.com", "trantuyen.dev.2@gmail.com", "trantuyen.dev.3@gmail.com",
+            "trantuyen.dev.4@gmail.com", "trantuyen.developer.981@gmail.com"};
+
+        public string[] arr_phone_receiver = { "0927042108", "1927142108", "2927242108", "3927342108", "4927442108" };
+
+        public string[] arr_address = { "256 LÊ VĂN QUỚI", "CƯ XÁ A KHU PHỐ 6", "TRƯỜNG ĐẠI HỌC NÔNG LÂM" };
+
+        public string[] arr_name_wards = {"PHƯỜNG 1", "PHƯỜNG 2", "PHƯỜNG 3", "PHƯỜNG 4", "PHƯỜNG 5",
+            "PHƯỜNG 6", "PHƯỜNG 7", "PHƯỜNG 8", "PHƯỜNG 9", "PHƯỜNG 10",
+            "PHƯỜNG 11", "PHƯỜNG 12", "PHƯỜNG 13", "PHƯỜNG 14", "PHƯỜNG 15"};
+
+        public string[] arr_id_wards = { "W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8", "W9", "W10", "W11", "W12", "W13", "W14", "W15" };
+
+        public string[] arr_name_districts = {"QUẬN 1", "QUẬN 2", "QUẬN 3", "QUẬN 4", "QUẬN 5",
+            "QUẬN 6", "QUẬN 7", "QUẬN 8", "QUẬN 9", "QUẬN 10",
+            "QUẬN 11", "QUẬN 12", "QUẬN 13", "QUẬN 14", "QUẬN 15",};
+
+        public string[] arr_id_districts = {"D1", "D2", "D3", "D4", "D5",
+            "D6", "D7", "D8", "D9", "D10",
+            "D11", "D12", "D13", "D14", "D15"};
+
+        public string[] arr_name_provinces = {"THÀNH PHỐ 1", "THÀNH PHỐ 2", "THÀNH PHỐ 3", "THÀNH PHỐ 4", "THÀNH PHỐ 5",
+            "THÀNH PHỐ 6", "THÀNH PHỐ 7", "THÀNH PHỐ 8", "THÀNH PHỐ 9", "THÀNH PHỐ 10",
+            "THÀNH PHỐ 11", "THÀNH PHỐ 12", "THÀNH PHỐ 13", "THÀNH PHỐ 14", "THÀNH PHỐ 15"};
+
+        public string[] arr_id_provinces = {"P1", "P2", "P3", "P4", "P5",
+            "P6", "P7", "P8", "P9", "P10",
+            "P11", "P12", "P13", "P14", "P15"};
+
+        public string[] arr_phone_senders = { "0379342981", "1379442981", "2379542981", "3379642981", "4379742981" };
+
+        public void AddDataToTable(MyDbContext dbContext)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Random random = new Random();
+            using (var transaction = dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    // tạo mới 10000 đơn hàng
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        int index_ward_from = random.Next(0, arr_name_wards.Length);
+                        int index_district_from = random.Next(0, arr_name_districts.Length);
+                        int index_province_from = random.Next(0, arr_name_provinces.Length);
+
+                        int index_ward_to = random.Next(0, arr_name_wards.Length);
+                        int index_district_to = random.Next(0, arr_name_districts.Length);
+                        int index_province_to = random.Next(0, arr_name_provinces.Length);
+
+                        // phí ship từ 0 cho đến 500 ngàn 
+                        var ship_price = random.Next(0, (int)(5 * Math.Pow(10, 5)));
+
+                        // đơn hàng có trị giá từ 0 cho đến 500 triệu đồng
+                        var order_value = random.Next(0, (int)(500 * Math.Pow(10, 6)));
+
+                        var order = new Order
+                        {
+                            from_name = arr_name_senders[random.Next(0, arr_name_senders.Length)],
+                            from_phone = arr_phone_senders[random.Next(0, arr_phone_senders.Length)],
+
+                            from_address = arr_address[random.Next(0, arr_address.Length)],
+                            from_ward_name = arr_name_wards[index_ward_from],
+                            from_ward_id = arr_id_wards[index_ward_from],
+                            from_district_name = arr_name_districts[index_district_from],
+                            from_district_id = arr_id_districts[index_district_from],
+                            from_province_name = arr_name_provinces[index_province_from],
+                            from_province_id = arr_id_provinces[index_province_from],
+
+                            to_name = arr_name_receivers[random.Next(0, arr_name_receivers.Length)],
+                            to_phone = arr_phone_receiver[random.Next(0, arr_phone_receiver.Length)],
+
+                            to_address = arr_address[random.Next(0, arr_address.Length)],
+                            to_ward_name = arr_name_wards[index_ward_to],
+                            to_ward_id = arr_id_wards[index_ward_to],
+                            to_district_name = arr_name_districts[index_district_to],
+                            to_district_id = arr_id_districts[index_district_to],
+                            to_province_name = arr_name_provinces[index_province_to],
+                            to_province_id = arr_id_provinces[index_province_to],
+
+                            email_customer = arr_email_customers[random.Next(0, arr_email_customers.Length)],
+
+                            ship_price = ship_price,
+                            order_value = order_value,
+                            total_price = ship_price + order_value,
+
+                            id_status_order = random.Next(1, 8) // id_status_order từ 1 đến 7
+                        };
+                        dbContext.Orders.Add(order);
+                    }
+                    dbContext.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            watch.Stop();
+            Console.WriteLine("Execute time: " + watch.Elapsed.TotalSeconds + " s");
+        }
     }
 }
---
+++ Migrations/20230804102139_updata_table_orders.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_Ban_Giay_Asp_Net_Core.Migrations
+{
+    public partial class updata_table_orders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "time_updated",
+                table: "Orders",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_ward_name",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_ward_id",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_province_name",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_province_id",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_phone",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_name",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_district_name",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_district_id",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_address",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "note",
+                table: "Orders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "time_updated",
+                table: "Orders",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_ward_name",
+                keyValue: null,
+                column: "return_ward_name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_ward_name",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_ward_id",
+                keyValue: null,
+                column: "return_ward_id",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_ward_id",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_province_name",
+                keyValue: null,
+                column: "return_province_name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_province_name",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_province_id",
+                keyValue: null,
+                column: "return_province_id",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_province_id",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_phone",
+                keyValue: null,
+                column: "return_phone",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_phone",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_name",
+                keyValue: null,
+                column: "return_name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_name",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_district_name",
+                keyValue: null,
+                column: "return_district_name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_district_name",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_district_id",
+                keyValue: null,
+                column: "return_district_id",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_district_id",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "return_address",
+                keyValue: null,
+                column: "return_address",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "return_address",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "note",
+                keyValue: null,
+                column: "note",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "note",
+                table: "Orders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
