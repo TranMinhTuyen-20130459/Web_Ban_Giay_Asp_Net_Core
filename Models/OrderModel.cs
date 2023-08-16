f4b67821751e429a3d3a94e9afec7af52d6d28d9
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-08-16 09:33:01 +0700

    dùng attribute anotation để validate data cho API (tạo mới một đơn hàng vào hệ thống)

--- Models/OrderModel.cs
+++ Models/OrderModel.cs
@@ -1,20 +1,28 @@
-﻿namespace Web_Ban_Giay_Asp_Net_Core.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Ban_Giay_Asp_Net_Core.Models
 {
     public class OrderModel
     {
 
+        [Required(ErrorMessage = "Customer name is required.")]
         public string name_customer { get; set; }
 
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be 10 digits.")]
         public string phone { get; set; }
 
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string? email_customer { get; set; }
 
         public AddressModel to_address { get; set; }
 
+        [MaxLength(200, ErrorMessage = "Note cannot exceed 200 characters.")]
         public string? note { get; set; }
 
+        [Range(0, Double.MaxValue, ErrorMessage = "Ship price must be >= 0")]
         public decimal ship_price { get; set; }
 
+        [Range(0, Double.MaxValue, ErrorMessage = "Order value must be >= 0")]
         public decimal order_value { get; set; }
 
         public List<OrderDetailModel> list_order_detail { get; set; }
