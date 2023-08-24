86f956331dca8089379ef0fbf7b15e8e8a474b47
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-08-24 10:58:56 +0700

    thêm class LoginModel và LoginResponse

---
+++ Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Ban_Giay_Asp_Net_Core.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        public string password { get; set; }
+    }
+}
---
+++ Models/Response/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Ban_Giay_Asp_Net_Core.Models.Response
+{
+    public class LoginResponse
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public long expires_in { get; set; }
+    }
+}
