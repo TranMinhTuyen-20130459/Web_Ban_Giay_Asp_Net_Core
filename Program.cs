0c97f755251af9371aee9aed7493d25062a6987a
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-08-29 09:30:17 +0700

    cấu hình để chạy Swagger trên Global Internet

--- Program.cs
+++ Program.cs
@@ -35,11 +35,16 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
