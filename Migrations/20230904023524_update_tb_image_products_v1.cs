fc0d9493997fb147c8de161262063e1c472ab14c
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-09-04 09:37:55 +0700

    thêm thuộc tính time_created trong entity ImageProduct

---
+++ Migrations/20230904023524_update_tb_image_products_v1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_Ban_Giay_Asp_Net_Core.Migrations
+{
+    public partial class update_tb_image_products_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "time_created",
+                table: "Image_Products",
+                type: "datetime(6)",
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "time_created",
+                table: "Image_Products");
+        }
+    }
+}
